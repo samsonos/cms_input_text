ca48f80d2ba55c611057e6d49298883d8c9341f2
omaximus <maxomelch93@gmail.com>
2015-03-25 20:12:23 +0200

    module created

---
+++ src/Text.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by Maxim Omelchenko <omelchenko@samsonos.com>
+ * on 25.03.2015 at 19:44
+ */
+namespace samsoncms\input\text;
+
+use \samsoncms\input\Field;
+
+class Text extends Field
+{
+    /** @var  int Field type identifier */
+    protected static $type = 0;
+
+    protected $id = 'samson_cms_input_text';
+
+    /** Database object field name */
+    protected $param = 'Value';
+
+    /** Special CSS classname for nested field objects to bind JS and CSS */
+    protected $cssClass = '__textarea';
+}
